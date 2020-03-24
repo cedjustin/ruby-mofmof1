0425050d20cacd9250c6314f71cb22e64b964eb1
root <root@DESKTOP-3IBCSAR.localdomain>
2020-03-24 17:06:11 +0200

    added create function

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -8,6 +8,15 @@ class PropertiesController < ApplicationController
         @property.stations.build
     end
 
+    def create
+        @property = Property.new(property_params)
+        if  @property.save
+            redirect_to  @property ,  notice: 'Property was successfully created.'
+        else
+            render  :new
+        end
+    end
+
     private
     def property_params
         params.require(:property).permit(:name, :rent, :address, :year, :remarks, stations_attributes: %i(id line name minute Property_id))
